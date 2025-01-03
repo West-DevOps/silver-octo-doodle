ef8149e8fa60e5398c205729c24f162aacbc9fee
Ben West <benjamin.west@bt.com>
2025-01-03 20:46:03 +0000

    just playing around now

--- nodejs/src/__tests__/bbc.ts
+++ nodejs/src/__tests__/bbc.ts
@@ -1,20 +1,39 @@
-import {Builder, Capabilities, By, WebElement} from 'selenium-webdriver';
+import {Builder, Capabilities, By} from 'selenium-webdriver';
 
 const capabilities = Capabilities.chrome();
 const driver = new Builder().withCapabilities(capabilities).build();
 
+/**
+ * This is a test 'suite', a group of logically related tests.
+ */
 describe('Test BBC news website homepage', () => {
-  driver.get('https://www.bbc.co.uk/news').then();
+  /**
+   * This will be called once before all tests are ran, seeing as we only need to request the page once and then are
+   * reading static elements off the page this is all we need to do.
+   */
+  beforeAll(async () => {
+    await driver.get('https://www.bbc.co.uk/news');
+  });
 
+  /**
+   * This is an actual 'test'
+   */
   it('Should be: Home - BBC News', async () => {
     expect(await driver.getTitle()).toBe('Home - BBC News');
   });
 
-  it('Should have the header with id header-content', async () => {
-    const loc = await driver.findElement(By.id('header-content'));
-    expect(loc).toBeInstanceOf(WebElement);
+  /**
+   * This is also an individual test
+   */
+  it('The header should contain the sports', async () => {
+    const header = await driver.findElement(By.id('header-content'));
+    const headerText = await header.getText();
+    expect(headerText).toContain('Sport');
   });
 
+  /**
+   * Close the browser down after all tests are done
+   */
   afterAll(() => {
     driver.quit();
   })
