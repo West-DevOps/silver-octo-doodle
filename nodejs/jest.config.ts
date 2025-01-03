32376e39897e5e4376223a1d9f7465c2c6345540
Ben West <benjamin.west@bt.com>
2025-01-03 20:05:16 +0000

    double the tests

--- nodejs/jest.config.ts
+++ nodejs/jest.config.ts
@@ -4,10 +4,7 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 const jestConfig : JestConfigWithTsJest = {
   bail: true,
   verbose: true,
-  collectCoverage: true,
-  testTimeout: 10000,
-  collectCoverageFrom: ['src/**/*.ts'],
-  coveragePathIgnorePatterns: ['\\.d\\.ts$'],
+  collectCoverage: false,
   modulePathIgnorePatterns: [
     'node_modules/',
     'dist/'
--- nodejs/src/__tests__/bbc.ts
+++ nodejs/src/__tests__/bbc.ts
@@ -1,12 +1,18 @@
-import { Builder, Capabilities, By } from 'selenium-webdriver';
+import {Builder, Capabilities, By, WebElement} from 'selenium-webdriver';
 
 const capabilities = Capabilities.chrome();
 const driver = new Builder().withCapabilities(capabilities).build();
 
-describe('Test BBC website title', () => {
+describe('Test BBC news website homepage', () => {
+  driver.get('https://www.bbc.co.uk/news').then();
+
   it('Should be: Home - BBC News', async () => {
-    await driver.get('https://www.bbc.co.uk/news');
     expect(await driver.getTitle()).toBe('Home - BBC News');
+  });
+
+  it('Should have the header with id header-content', async () => {
+    const loc = await driver.findElement(By.id('header-content'));
+    expect(loc).toBeInstanceOf(WebElement);
   });
 
   afterAll(() => {
